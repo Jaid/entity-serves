02d7a0c99baf6de5c2c9918ae7c66429d1330a20
Jaid <jaid.jsx@gmail.com>
2020-01-06 09:51:25 +0100

    Removed constructor options from `jaid-core-twitch-auth`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/plugins/twitchAuth/index.js
+++ src/plugins/twitchAuth/index.js
@@ -1,6 +1,3 @@
 import TwitchAuthPlugin from "jaid-core-twitch-auth"
 
-export default new TwitchAuthPlugin({
-  domain: "server.entity.works",
-  successRedirect: "https://e.com",
-})
+export default new TwitchAuthPlugin
