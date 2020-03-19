05d6653ba38fe3fb9bb51e861ede5ba97793fca8
Jaid <jaid.jsx@gmail.com>
2020-03-19 16:42:58 +0100

    feat: Added toast for successfully created builds
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/plugins/socketServer/commands/addBuild.js
+++ src/plugins/socketServer/commands/addBuild.js
@@ -10,5 +10,6 @@ export default async (context, payload) => {
   }
   const user = await User.findByPk(context.userId)
   const build = await Build.addBuild(payload.formType, user.id, payload.formData)
+  context.client.emit("toast", "Your build has been published")
   return pick(build, ["id", "seoLinkId"])
 }
